b6965c5d96bede205a17ec0409116cb16c1b7ba6
nimish1995 <nimish1995@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-31 19:27:10 +0000

    Tesing
    git-svn-id: https://plugins.svn.wordpress.org/the-ultimate-live-tracking-feed/trunk@1124759 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -11,6 +11,15 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 delete_option('live-nimlinks');
+delete_option( 'live_BorderRadius');
+delete_option( 'live_Width');
+delete_option( 'live_BackgroundColor');
+delete_option( 'live_BorderColor');
+delete_option( 'live_TextColor');
+delete_option( 'live_OnlineSize');
+delete_option( 'live_CounterValuesSize');
+delete_option( 'live_CounterHeadingSize');
+delete_option( 'live_Line');
 
 function remove_LiveTracker_widget() {
 	unregister_widget('LiveTracker');
